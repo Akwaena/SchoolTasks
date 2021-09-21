06d12d69ff6d30d598538dfe54e98e1cbaa577e5
Akwaena <74779726+Akwaena@users.noreply.github.com>
2021-09-21 06:53:56 +0300

    Add files via upload

--- PostPreInfix calc.py
+++ PostPreInfix calc.py
@@ -1,74 +1,174 @@
 operators = {
-    '+': lambda x, y: x + y,
-    '-': lambda x, y: x - y,
-    '*': lambda x, y: x * y,
-    '/': lambda x, y: y // x
+    '+': (lambda x, y: x + y, 2),
+    '-': (lambda x, y: x - y, 2),
+    '*': (lambda x, y: x * y, 1),
+    '/': (lambda x, y: y // x, 1)
 }
 
 
 def input_check(input_expression):
     if not input_expression:
-        print('Выражение пусто (Err1)')
-        exit(0)
+        print('Выражение пусто')
+        return False
 
     if len(input_expression.split()) < 3:
-        print('Выражение некорректно (Err2)')
-        exit(0)
+        print('Выражение содержит недостаточно элементов')
+        return False
 
     for i in input_expression:
         if i not in operators.keys() and not i.isdigit() and not ' ':
-            print(f'Выражение содержит некорректный символ "{i}" (Err3)')
-            exit(0)
+            print('Выражение содержит некорректный символ "{}"'.format(i))
+            return False
 
-    temp = [0, 0]
-    for i in input_expression:
-        if i.isdigit():
-            temp[0] += 1
-        elif i in operators.keys():
-            temp[1] += 1
-    if temp[0] / 2 != temp[1]:
-        print('Выражение некорректно (Err4)')
-        exit(0)
+    #    temp = [0, 0]
+    #    for i in input_expression:
+    #        if i.isdigit():
+    #            temp[0] += 1
+    #        elif i in operators.keys():
+    #            temp[1] += 1
+    #    if temp[0] / 2 != temp[1]:
+    #        raise Exception('Недостаточно операторов')
 
     print('Проверка корректности выражения пройдена')
+    return True
 
 
-def calculate_infix_exp(input_expression):
-    print('Расчет инфиксного выражения')
-    print(int(eval(input_expression)))
-
-
-def calculate_postfix_exp(input_expression):
-    print('Расчет постфиксного выражения')
-    returned = list()
-    for char in input_expression.split():
-        if char.isdigit():
-            returned.append(int(char))
+def exp_type(expression):
+    print('Вычисляем тип выражения')
+    if input_check(expression):
+        if expression[0] in operators.keys():
+            return 'prefix_type'
+        elif expression[-1] in operators.keys():
+            return 'postfix_type'
         else:
-            returned.append(operators[char](returned.pop(), returned.pop()))
-
-    print(returned[0])
+            return 'infix_type'
 
 
-def calculate_prefix_exp(input_expression):
-    print('Расчет богомерзкого префиксного выражения')
-    pass
+def calculate_exp(input_expression):
+    print('Расчет выражения')
+
+    if input_expression[0] in operators.keys():
+        returned = pre_to_inf(input_expression)
+    elif input_expression[-1] in operators.keys():
+        returned = post_to_inf(input_expression)
+    else:
+        returned = input_expression
+    print('Расчет {}'.format(returned))
+    return eval(returned)
+
+
+def post_to_inf(post_exp):
+    print('Конвертация Постфикс в Инфикс  {}'.format(post_exp))
+    expression, stack = post_exp.split(), list()
+
+    for i in expression:
+        if i.isdigit():
+            stack.append(i)
+        elif i in operators.keys():
+            stack.append('({} {} {})'.format(stack.pop(-2), i, stack.pop()))
+
+    return stack[-1]
+
+
+def pre_to_inf(pre_exp):
+    print('Конвертация Префикс в Инфикс  {}'.format(pre_exp))
+    expression = pre_exp.split()
+    while len(expression) != 1:
+        pose_op = 0
+        for num in range(len(expression)):
+            if expression[num] in operators.keys():
+                pose_op = num
+        part = '( ' + str(expression[pose_op + 1]), str(expression[pose_op]), str(expression[pose_op + 2]) + ' )'
+        expression[pose_op + 2] = part
+        del expression[pose_op:pose_op + 2]
+    return str(expression[0])
+
+
+def inf_to_post(inf_exp):
+    print('WIP')
+    return
+
+
+def inf_to_pre(inf_exp):
+    print('WIP')
+    return
+
+
+def convert(expression, to_type):
+    if input_check(expression):
+        if exp_type(expression) == 'infix_type':
+            if to_type == 'префикс':
+                pass
+            elif to_type == 'постфикс':
+                inf_to_post(expression)
+            else:
+                return expression
+        elif exp_type(expression) == 'prefix_type':
+            if to_type == 'инфикс':
+                return pre_to_inf(expression)
+            elif to_type == 'постфикс':
+                inf_to_post(None)
+            else:
+                return expression
+        elif exp_type(expression) == 'postfix_type':
+            if to_type == 'инфикс':
+                return post_to_inf(expression)
+            elif to_type == 'префикс':
+                pass
+            else:
+                return expression
+
+
+def args_check(commands, args):
+    if len(commands) != args:
+        print('Недостаточно аргументов к команде')
+        return False
+    else:
+        return True
 
 
 def main():
-    input_expression = input('Введите выражение: ')
-    input_check(input_expression)
-    if input_expression[0] in operators.keys():
-        calculate_prefix_exp(input_expression)
-    elif input_expression[-1] in operators.keys():
-        calculate_postfix_exp(input_expression)
-    else:
-        calculate_infix_exp(input_expression)
+    print('Калькулятор и преобразователь Инф/Пост/Префиксных выражений')
+    print('Для получения списка команд введите "помощь", "справка" или "команды"')
+    while True:
+        commands = input('Введите команду: ').split(' /')
 
-    print()
+        if commands[0].lower() in ('выйти', 'выход'):
+            print('Выход...')
+            exit(0)
+        elif commands[0].lower() in ('вычислить', 'посчитать'):
+            print('Определена команда на расчет выражения')
+            if args_check(commands, 2):
+                if input_check(commands[1]):
+                    print('Ответ: ', calculate_exp(commands[1]))
+        elif commands[0].lower() in ('определить', 'определить тип'):
+            print('Определена команда на определение типа выражения')
+            if args_check(commands, 2):
+                if exp_type(commands[1]) == 'infix_type':
+                    print('Выражение записано в инфиксной нотации')
+                elif exp_type(commands[1]) == 'prefix_type':
+                    print('Выражение записано в префиксной нотации')
+                elif exp_type(commands[1]) == 'postfix_type':
+                    print('Выражение записано в постфиксной нотации')
+        elif commands[0] in ('перевести', 'конвертировать'):
+            print('Определена команда на конвертацию выражения')
+            if args_check(commands, 3):
+                print('Ответ:', convert(commands[1], commands[2]))
+        elif commands[0].lower() in ('помощь', 'команды', 'справка'):
+            print('помощь/команды/справка -- получить список команд и справку')
+            print('выход/выйти -- покинуть программу')
+            print('определить/определить тип /{выражение} -- определяет нотацию выражения')
+            print('вычислить/посчитать /{выражение} -- вычисляет заданное выражение')
+            print('перевести/конвертировать /{выражение} /{тип} -- переводит выражение в заданный тип\n')
+            print('Оператор математических выражений различных нотаций')
+            print('Функционал включает в себя: конвертацию выражений в другие нотации, вычисление выражений различной '
+                  'нотации, определение нотации выражения\n')
+            print('Версия 1.2')
+            print('Разработчик -- Akwaena')
+            print('По заказу Зелениной С.Б.')
+            print('Сентябрь 2021')
+
+        print()
 
 
-print('Калькулятор Инф/Пост/Префиксных выражений')
-
-while True:
-    main()
+main()
