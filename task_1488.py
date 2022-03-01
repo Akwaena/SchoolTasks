b4f67c8819324720242f6f36b530f6c1aaf51bfd
Akwaena <74779726+Akwaena@users.noreply.github.com>
2022-03-01 13:17:47 +0300

    Add files via upload

---
+++ task_1488.py
@@ -0,0 +1,27 @@
+def conunction(n, k):
+    n = bin(n)[2:]
+    k = bin(k)[2:]
+    s = ''
+    if len(n) != len(k):
+        if len(n) > len(k):
+            k = '0' * (len(n) - len(k)) + k
+        else:
+            n = '0' * (len(k) - len(n)) + n
+    for i in range(len(n)):
+        if n[i] == k[i] == '1':
+            s += '1'
+        else:
+            s += '0'
+    return int(s,2)
+
+def implication(x, y): return not x or y
+
+def f(A):
+    for x in range(10000):
+        if not implication(conunction(x, 41) == 0, implication(conunction(x, 119) != 0,conunction(x, A) != 0)):
+            return False
+    return A
+
+
+for i in range(100):
+    if F(i)!=False: print(i)
